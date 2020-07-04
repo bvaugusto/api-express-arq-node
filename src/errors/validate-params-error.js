f367cf1d7b263917af5f48babbd4eab5572263fb
Bruno <bvaugusto@gmail.com>
2020-07-04 13:00:36 -0300

    Fix - validate numeric params

---
+++ src/errors/validate-params-error.js
@@ -0,0 +1,1 @@
+module.exports = class ValidateParamsError extends Error {}
--- src/services/calculator-service.js
+++ src/services/calculator-service.js
@@ -1,11 +1,16 @@
 const arqCalculator = require('arq-calculator');
-const CalculatorError = require('../errors/calculator-error')
+const CalculatorError = require('../errors/calculator-error');
+const ValidateParamsError = require('../errors/validate-params-error');
 
 class calculatorService {
 
   async calculator(params) {
     const { operator } = params;
     let { param1, param2 } = params;
+
+    if (!(typeof param1 === "number" && typeof param2 === "number")) {
+      throw new ValidateParamsError("É permitido somente valor numérico!");
+    }
     
     param1 = parseInt(param1, 10);
     param2 = parseInt(param2, 10);
